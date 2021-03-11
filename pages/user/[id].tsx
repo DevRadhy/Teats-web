e3d34c0e80a69063a85aa1d3439908dbe893046e
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2021-03-10 22:36:38 -0300

    create FoodSettings page

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -1,20 +1,47 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { Header } from '../../src/components/Header';
+import { FoodCard } from '../../src/components/FoodCard';
+
+import styles from '../../src/styles/pages/Settings.module.css';
 
 export default function Settings() {
   const router = useRouter();
   const { id } = router.query;
 
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Teats | { id }</title>
       </Head>
 
-      <Header
-        text={`Olá, ${id}`}
-      ></Header>
+      <header>
+        <article><img src="/icons/arrow-left.svg" alt="Arrow left"/></article>
+
+        <p>Aqui você está no controle, CUIDADO! essa página tem Poderes de Super Vaca.</p>
+
+        <img src="https://github.com/DevRadhy.png" alt="Profile"/>
+
+        <span>MOOOO...</span>
+
+        <article><img src="/icons/filter.svg" alt="Filter"/></article>
+      </header>
+
+      <section>
+        <div className={styles.gridContainer}>
+          <FoodCard />
+          <FoodCard />
+          <FoodCard />
+          <FoodCard />
+          <FoodCard />
+          <FoodCard />
+          <FoodCard />
+          <FoodCard />
+          <FoodCard />
+          <FoodCard />
+          <FoodCard />
+          <FoodCard />
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/FoodCard.tsx
+++ src/components/FoodCard.tsx
@@ -5,7 +5,7 @@ export function FoodCard() {
     <div className={styles.container}>
       <div className={styles.gridContainer}>
         <div>
-          <img src="https://images.unsplash.com/photo-1520201163981-8cc95007dd2a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" alt="Product"/>
+          <img src="https://images.unsplash.com/photo-1520201163981-8cc95007dd2a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D.png" alt="Product"/>
         </div>
 
         <div>
