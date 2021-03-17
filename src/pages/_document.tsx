93bbcac7c31e58a65b805b7ee40b80a7db02766b
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2021-03-17 16:37:59 -0300

    Adicionando fonte Roboto

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,10 +9,11 @@ export default class MyDocument extends Document {
           <meta name="description" content="Coffee break para todos relaxarem e se divertirem com aquele lanche especial."/>
 
           <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
-          <link 
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=Ubuntu:wght@700&family=Roboto:wght@600&display=swap"
+          <link rel="preconnect" href="https://fonts.gstatic.com"/>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=Roboto:wght@400;700&display=swap"
             rel="stylesheet"
-          ></link>
+          />
         </Head>
         <body>
           <Main />
